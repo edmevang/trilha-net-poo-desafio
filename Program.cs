38e71a8a627f4f4fb90f1cd87ba433f90d06a03e
edmevang <edmevang@yahoo.com.br>
2022-10-30 01:00:40 -0300

    atualizações

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "98765", modelo: "E-51", imei: "nk123456", memoria: 5);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("-------------");
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "11112222333", modelo: "Iphone 13X", imei: "iph50002000", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
+
